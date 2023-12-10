79bdc7ab7a1b3bdcc2daaa2c88307b1210275c29
Gabriele Di Grazia <gabrii539@gmail.com>
2023-12-10 23:11:01 +0100

    feat(localstorage): inseriti i metodi getClasse() e e removeStudente()

--- js/localstorage.js
+++ js/localstorage.js
@@ -15,12 +15,20 @@ export function getClassi(){
     return classi;
 }
 
+export function getClasse(classe){
+    let y = JSON.parse(localStorage.getItem(classe.nome));
+    y.inserisci = RegistroClasse.prototype.inserisci;
+    console.log(y);
+    return y;
+}
+
+
 export function setClasse(classe){
     //console.log(classe.nome);
     localStorage.setItem(classe.nome, JSON.stringify(classe));
 }
 
-setClasse(new RegistroClasse('E', 1));
+//setClasse(new RegistroClasse('E', 1));
 
 export function insertStudente(nome, cognome){
     let classe = JSON.parse(localStorage['1˚E']);
@@ -29,7 +37,13 @@ export function insertStudente(nome, cognome){
     setClasse(classe);
 }
 
-insertStudente('lorenzo', 'Di Grazia');
+export function removeStudente(classe, id){
+    let newClasse = JSON.parse(localStorage[classe.nome]);
+    delete newClasse.elenco[id];
+    localStorage[classe.nome] = JSON.stringify(newClasse);
+}
+
+//insertStudente('lorenzo', 'Di Grazia');
 
 
 setClasse(new RegistroClasse('E', 2));
