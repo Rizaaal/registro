8fbefe3c7b661dad4f5fcd057d217fdf7b19b6f3
Gabriele Di Grazia <gabrii539@gmail.com>
2023-12-10 23:13:45 +0100

    feat: added navigation to edit page on registro page

--- js/model/pagine/registro.js
+++ js/model/pagine/registro.js
@@ -1,13 +1,24 @@
 // import { registro } from "../../view.js"; //table
+import { edita } from "./edita.js";
+import { showPage } from "../../view.js";
 
+//il valore del parametro lo prende da classi.js
 export function registroPagina(classe){
 
     const table = document.createElement('table');
-    console.log(classe);
 
+    const editButton = document.createElement('button');
+    editButton.innerText = 'edita';
+    editButton.addEventListener('click', e => {
+        showPage(edita, classe);
+    })
+    table.append(editButton);
+
+    // console.log(classe);
     for (const key in classe.elenco) {
         if (classe.elenco.hasOwnProperty(key)) {
             const tr = document.createElement('tr');
+
             let nome = document.createElement('td');
             nome.innerHTML = classe.elenco[key].nome;
             tr.appendChild(nome);
